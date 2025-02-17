6c19ab6889ad64b389833abf912b77e3026bd09e
caaarolb <carolinelboaventura@gmail.com>
2025-02-17 16:49:32 -0300

    ✨ Cadastro e Login ✨

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,43 +1,34 @@
 function Home() {
     return (
-        <>
-            <div style={{
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center",
-                backgroundColor: "rgba(255,192,203)"    
-            }}>
-                <div>
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center",
-                         backgroundColor: "rgba(219,112,147)",
-                         borderRadius: "20px",
-                        fontFamily: "Arial, sans-serif"
-                    }}>
-                        <h2>⭐ Blog Pessoal ⭐</h2>
-                        <p>Expresse aqui seus pensamentos e opiniões</p>
+        <div className="flex justify-center bg-indigo-900">
+            <div className="container grid grid-cols-2 text-white">
+                <div className="flex flex-col items-center justify-center gap-4 py-4">
+                    <h2 className="text-5xl font-bold">
+                        Seja Bem-Vindo!
+                    </h2>
+                    <p className="text-xl">
+                        Expresse aqui seus pensamentos e
+                        opiniões
+                    </p>
+
+                    <div className="flex justify-around gap-4">
+                        <div className="rounded border-white border-solid border-2 py-2 px-4 text-white">
+                            Nova Postagem
+                        </div>
                     </div>
 
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                         
-                    }}>
-                        <img 
-                            src="https://i.imgur.com/Xz1IrCf.png" 
-                            alt="Imagem da Página Home" 
-                            width="400px"
-                        />
-                    </div>
+                </div>
+ 
+                <div className="flex justify-center">
+                    <img
+                        src="https://i.imgur.com/VpwApCU.png"
+                        alt="Imagem da Página Home"
+                        className="w-2/3"
+                    />
                 </div>
             </div>
-        </>
+        </div>
     )
 }
-
+ 
 export default Home
